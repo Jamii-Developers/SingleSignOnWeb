e6b84afd80d96181fbda9960864adf44dd2a185f
michael-ayako <56703374+michael-ayako@users.noreply.github.com>
2023-09-21 05:50:56 -0500

    Updating contactus

--- src/contactus.js
+++ src/contactus.js
@@ -1,4 +1,6 @@
 import './sass/contactus.sass';
+import ServerErrorMsg from './frequentlyUsedModals/servererrormsg';
+import ServerSuccessMsg from './frequentlyUsedModals/serversuccessmsg';
 
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
@@ -10,13 +12,33 @@ import Alert from '@mui/material/Alert';
 
 
 const ContactUs = ( ) => {
+
+      const [serverErrorCode, setServerErrorCode ] = useState("");
+      const [serverErrorSubject, setServerErrorSubject ] = useState("");
+      const [serverErrorMessage, setServerErrorMessage ] = useState("");
+      const [errServMsgShow, setErrServMsgShow ] = useState(false);
+
+      const [ui_subject, setUi_subject ] = useState("");
+      const [ui_message, setUi_message ] = useState("");
+      const [succServMsgShow, setSuccServMsgShow ] = useState(false);
+
+      const [ email, setEmail ] = useState("") ;
+      const [ username, setUsername ] = useState("") ;
+      const [ thoughts, setThoughts ] = useState("") ;
+
       function PageUnderDevelopmentNotice(  ){
             return( 
-                
-                <Alert variant="filled" severity="info" className='mb-3' >Page under development</Alert>   
-                                
+                <Alert variant="filled" severity="info" className='mb-3' >Page under development</Alert>            
             );
       }
+
+      function ShowEmailError(  ){
+            return( 
+                <Collapse in ={ errordata.loginCredentialErrorTrigger }>
+                    <Alert variant="filled" severity="warning" className='mb-3' >{ errordata.loginCredentialErrorMessage }</Alert>   
+                </Collapse>                    
+            );
+        }
 
       return (
             <div id = "ContactUsPage">
@@ -29,16 +51,22 @@ const ContactUs = ( ) => {
                   <p>At Jamii developers as we aim to improve and grow our solutions we appreciate any feedback in form of complements or complaints provided to us.</p>
 
                   <FloatingLabel controlId="email" label="Email address" className="mb-3">
-                        <Form.Control type="email" placeholder="name@example.com" />
+                        <Form.Control id = "email" type="email" placeholder="name@example.com" />
                   </FloatingLabel>
+
+                  <ShowEmailError />
 
                   <FloatingLabel controlId="username" label="Username" className="mb-3">
-                        <Form.Control type="text" placeholder="Username" />
+                        <Form.Control id = "username" type="text" placeholder="Username" />
                   </FloatingLabel>
 
-                  <FloatingLabel controlId="review" label="Leave your thoughts here" className="mb-3" >
-                        <Form.Control as="textarea" placeholder="Leave your thoughts here" style={ { height: '100px' } } />
+                  <ShowUsernameError />
+
+                  <FloatingLabel controlId="thoughts" label="Leave your thoughts here" className="mb-3" >
+                        <Form.Control id = "thoughts" as="textarea" placeholder="Leave your thoughts here" style={ { height: '100px' } } />
                   </FloatingLabel>
+
+                  <ShowThoughtsError />
 
                   <ButtonGroup size="md" className="mb-2">
                         <Button variant="outline-primary" type="submit">Send</Button>
@@ -46,6 +74,21 @@ const ContactUs = ( ) => {
                   </ButtonGroup>
                   
             </Form>
+
+            < ServerErrorMsg 
+                    open={errServMsgShow}  
+                    onClose={ ( ) => setErrServMsgShow( false )  }
+                    errorcode = {serverErrorCode} 
+                    errorsubject = {serverErrorSubject} 
+                    errormessage = {serverErrorMessage}                             
+                />
+
+                < ServerSuccessMsg 
+                    open={succServMsgShow}  
+                    onClose={ ( ) => setSuccServMsgShow( false )  }
+                    ui_subject = {ui_subject} 
+                    ui_message = {ui_message}                             
+                />
             
           </div>
       )
