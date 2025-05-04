bac5571a152e3204561193eb43bcc793ebaf25b5
michael-ayako <56703374+michael-ayako@users.noreply.github.com>
2025-05-03 21:57:58 -0500

    Update followers.jsx

--- src/myHomeComponents/SocialComponents/followers.jsx
+++ src/myHomeComponents/SocialComponents/followers.jsx
@@ -8,8 +8,8 @@ import '../sass/followers.sass';
 import JsonNetworkAdapter from '../../configs/networkadapter';
 import conn from '../../configs/conn';
 import constants from '../../utils/constants';
-import ServerErrorMsg from '../../frequentlyUsedModals/ServerErrorMsg';
-import ServerSuccessMsg from '../../frequentlyUsedModals/ServerSuccessMsg';
+import ServerErrorMsg from '../../frequentlyUsedModals/servererrormsg';
+import ServerSuccessMsg from '../../frequentlyUsedModals/serversuccessmsg';
 
 const Followers = () => {
     const [cookies] = useCookies("userSession");
